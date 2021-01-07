61152cafc913b42d932fbc1f2d49a7369ed65ad7
MostafaMukhtar <76975461+MostafaMukhtar@users.noreply.github.com>
2021-01-07 19:03:50 +0200

    add ROTATE procedure

--- Stepper motor project/FirstIdea.asm
+++ Stepper motor project/FirstIdea.asm
@@ -1,14 +1,14 @@
 .MODEL SMALL
 .DATA
 
-PORTA  = 00H                                         ;Address of Port A
+PORTA  = 00H                                                ;Address of Port A
        PORTB  = 02H                                         ;Address of Port B
        PORTC  = 04H                                         ;Address of Port C
-       CONTW  = 06H
+       CONTW  = 06H                                         ;Address of control word
 
 .CODE
 .STARTUP
-
+MOV SI,0
 
 
 
@@ -16,6 +16,16 @@ MAIN PROC
 
 MAIN ENDP
 
+ROTATE PROC
+          MOV AL,ROTATION[SI]
+          INC SI 
+          CMP SI,8
+          JNZ FINISH
+          MOV SI,0
+      FINISH:
+          OUT PORTA,AL
+          RET
+ ROTATE ENDP 
 
 
 
