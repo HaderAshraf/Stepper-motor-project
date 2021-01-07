57fd6d71295094c34a61f1abad60db57ac3b944f
SalahRasmy <77000346+SalahRasmy@users.noreply.github.com>
2021-01-07 18:30:16 +0200

    Update FirstIdea.asm

--- Stepper motor project/FirstIdea.asm
+++ Stepper motor project/FirstIdea.asm
@@ -1,6 +1,10 @@
 .MODEL SMALL
 .DATA
 
+PORTA  = 00H                                         ;Address of Port A
+       PORTB  = 02H                                         ;Address of Port B
+       PORTC  = 04H                                         ;Address of Port C
+       CONTW  = 06H
 
 .CODE
 .STARTUP
