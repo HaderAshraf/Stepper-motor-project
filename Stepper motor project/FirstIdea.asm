72fc23f9d4db12acebade29b9be0b44cf6521fc4
MostafaMukhtar <76975461+MostafaMukhtar@users.noreply.github.com>
2021-01-07 19:06:08 +0200

    Update FirstIdea.asm

--- Stepper motor project/FirstIdea.asm
+++ Stepper motor project/FirstIdea.asm
@@ -5,6 +5,18 @@ PORTA  = 00H                                                ;Address of Port A
        PORTB  = 02H                                         ;Address of Port B
        PORTC  = 04H                                         ;Address of Port C
        CONTW  = 06H                                         ;Address of control word
+      
+      ROTATION DB   00000001b				    ;Complete rotation matrix 8 steps
+		DB   00000011b
+		DB   00000010b
+		DB   00000110b
+		DB   00000100b
+		DB   00001100b
+		DB   00001000b
+		DB   00001001b
+
+
+
 
 .CODE
 .STARTUP
