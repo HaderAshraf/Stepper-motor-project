11acccd32fa1867ecb6beb5ac99dd35d6c3ad429
Hader Ashraf <42392736+HaderAshraf@users.noreply.github.com>
2021-01-07 20:21:46 +0200

    Update FirstIdea.asm

--- Stepper motor project/FirstIdea.asm
+++ Stepper motor project/FirstIdea.asm
@@ -5,7 +5,7 @@ PORTA  = 00H                                                ;Address of Port A
        PORTB  = 02H                                         ;Address of Port B
        PORTC  = 04H                                         ;Address of Port C
        CONTW  = 06H                                         ;Address of control word
-      
+      DELAY_TIME DW 06FFH
       ROTATION DB   00000001b				    ;Complete rotation matrix 8 steps
 		DB   00000011b
 		DB   00000010b
@@ -27,6 +27,15 @@ MOV SI,0
 MAIN PROC
 
 MAIN ENDP
+
+DELAY PROC								
+	 MOV BX, CX
+	 MOV CX, DELAY_TIME
+      delay1:
+	 loop delay1
+	 MOV CX, BX
+	 RET
+DELAY ENDP
 
 ROTATE PROC
           MOV AL,ROTATION[SI]
