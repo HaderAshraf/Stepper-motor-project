529802050f89d08ff3924c87d9ac325b90c80a5f
Mhamdmahmoud <76977145+Mhamdmahmoud@users.noreply.github.com>
2021-01-08 00:20:09 +0200

    ADDING SEVEN SEGMENTS DISPLAY

--- Stepper motor project/FirstIdea.asm
+++ Stepper motor project/FirstIdea.asm
@@ -1,12 +1,12 @@
 .MODEL SMALL
 .DATA
 
-PORTA  = 00H                                                ;Address of Port A
+       PORTA  = 00H                                                ;Address of Port A
        PORTB  = 02H                                         ;Address of Port B
        PORTC  = 04H                                         ;Address of Port C
        CONTW  = 06H                                         ;Address of control word
-      DELAY_TIME DW 06FFH
-      ROTATION DB   00000001b				    ;Complete rotation matrix 8 steps
+       DELAY_TIME DW 06FFH
+       ROTATION DB   00000001b				    ;Complete rotation matrix 8 steps
 		DB   00000011b
 		DB   00000010b
 		DB   00000110b
@@ -14,7 +14,12 @@ PORTA  = 00H                                                ;Address of Port A
 		DB   00001100b
 		DB   00001000b
 		DB   00001001b
-
+		
+	START_FLAG DB  00000000B
+	
+	DISPLAY DB   01101101b
+		DB   01110110b
+		DB   01110001b
 
 
 
@@ -52,6 +57,18 @@ ROTATE PROC
           OUT PORTA,AL
           RET
  ROTATE ENDP 
+
+CHAR_DISPLAY PROC
+MOV DL,AL
+MOV BX,0
+MOV BL,START_FLAG
+MOV AL,DISPLAY[BX]
+OUT PORTC,AL
+MOV AL,DL
+RET
+CHAR_DISPLAY ENDP
+
+
 
 
 
