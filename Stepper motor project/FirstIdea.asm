0fef014a36df35f231043248830e704e2eb43e28
MichaelMagdy3 <76997997+MichaelMagdy3@users.noreply.github.com>
2021-01-09 19:49:56 +0200

    Updating Comments.

--- Stepper motor project/FirstIdea.asm
+++ Stepper motor project/FirstIdea.asm
@@ -1,7 +1,7 @@
 .MODEL SMALL
 .DATA
 
-       PORTA  = 00H                                                ;Address of Port A
+       PORTA  = 00H                                         ;Address of Port A
        PORTB  = 02H                                         ;Address of Port B
        PORTC  = 04H                                         ;Address of Port C
        CONTW  = 06H                                         ;Address of control word
@@ -15,18 +15,18 @@
 		DB   00001000b
 		DB   00001001b
 		
-	START_FLAG DB  00000000B
+	START_FLAG DB  00000000B				;Initializing START_FLAG
 	
-	DISPLAY DB   01101101b                             ;display matrix for seven segment
+	DISPLAY DB   01101101b                             	;Display matrix for seven segment
 		DB   01110110b
 		DB   01110001b
 
 .CODE
 .STARTUP
 
-MOV AL,10000010B     ;PORTA & PORTC OUTPUT , PORTB INPUT , MODE 0
+MOV AL,10000010B     ;PORT A & PORT C OUTPUT , PORT B INPUT , MODE 0
 OUT CONTW,AL
-CALL CHAR_DISPLAY    ;display s char at the begin START_FLAG=0
+CALL CHAR_DISPLAY    ;Display 'S' char at the beginning START_FLAG=0
 MOV SI,0
 
 MAIN:
@@ -38,29 +38,29 @@ STOP:
 CALL TEST_SWITCHES	;check the input switches
 JMP MAIN		;loop for ever
 
-HALF_SPEED PROC
+HALF_SPEED PROC			;Defining Motor HALF_SPEED Function
 	 MOV START_FLAG,1
-	 CALL CHAR_DISPLAY
+	 CALL CHAR_DISPLAY	;Display 'H' Char
 	 MOV BX,07FEH
 	 MOV DELAY_TIME,BX
 	 RET
 HALF_SPEED ENDP
 
-FULL_SPEED PROC
+FULL_SPEED PROC			;Defining Motor FULL_SPEED Function
 	 MOV START_FLAG,2
-	 CALL CHAR_DISPLAY
+	 CALL CHAR_DISPLAY	;Display 'F' Char 
 	 MOV BX,03FFH
 	 MOV DELAY_TIME,BX
 	 RET
 FULL_SPEED ENDP
 
-STOPING PROC
+STOPING PROC			;Defining STOPING Function
 	 MOV START_FLAG,0
-	 CALL CHAR_DISPLAY
+	 CALL CHAR_DISPLAY	;Display 'S' Char
 	 RET
 STOPING ENDP
 
-DELAY PROC			;delay specific time depends on DELAY_TIME variable					
+DELAY PROC			;Delay specific time depends on DELAY_TIME variable					
 	 MOV BX, CX
 	 MOV CX, DELAY_TIME
       delay1:
